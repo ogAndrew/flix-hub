ea1872c7ce27ab5a071c7c3ebdeadbeca50ab55f
Andrew Talle <talle.andrew@gmail.com>
2021-07-28 13:25:28 -0600

    update action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,4 +11,11 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find(params[:id])
   end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+    @movie.update(movie_params);
+    redirect_to movie_path(@movie)
+  end
 end
