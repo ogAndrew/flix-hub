acc9f5fda74109197e8ef83c0afa6519374a34ae
Andrew Talle <talle.andrew@gmail.com>
2021-07-30 11:08:37 -0600

    add reviews index

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :movie
 
-  validates :name, presence: true 
+  validates :name, presence: true
 
   validates :comment, length: { minimum: 4 }
 
@@ -10,5 +10,9 @@ class Review < ApplicationRecord
   validates :stars, inclusion: {
     in: STARS,
     message: "must be between 1 and 5"
-  } 
+  }
+
+  def stars_as_percent
+    (stars / 5.0) * 100.0
+  end
 end
